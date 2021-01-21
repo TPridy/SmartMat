68846fe9f0baba18b2cb50c2e5a293d79d12e9f3
TPridy <32938581+TPridy@users.noreply.github.com>
2021-01-20 21:27:11 -0800

    Organizational commit

---
+++ WiFi Module/inc/settings.h
@@ -0,0 +1,14 @@
+/*
+  settings.h - Library for settings to be used with SmartMat.
+  Created by Thomas C Pridy, January 21st, 2021.
+  Released into the public domain.
+*/
+
+#ifndef Settings_h
+#define Settings_h
+#include "Arduino.h"
+
+
+
+
+#endif
